b248ed74962eefa1f0b7b42f68a7044d3e1b139c
Vanderson Faria Milagre <VandersonMilagre@hotmail.com.br>
2024-02-07 19:37:53 -0300

    Garantida Exibição Correta de Time Record Editing Histories

--- lib/models/widgets/time_record_editing_ch_model.dart
+++ lib/models/widgets/time_record_editing_ch_model.dart
@@ -1,10 +1,11 @@
 import 'package:projeto_time_counter/enums/time_record_editing_command.dart';
 import 'package:projeto_time_counter/models/widgets/reusable/command_history_model.dart';
 import 'package:projeto_time_counter/services/time_conversion_service.dart';
+import 'package:projeto_time_counter/utils/date_time_utils.dart';
 
 class TimeRecordEditingChModel extends CommandHistoryModel{
   final TimeRecordEditingCommand _command;
-  final dynamic _updateInfo;
+  final Map<String, dynamic> _updateInfo;
 
   @override
   String get commandName => _command.name;
@@ -12,31 +13,31 @@ class TimeRecordEditingChModel extends CommandHistoryModel{
   @override
   dynamic get updateInfo => _updateInfo;
 
-  TimeRecordEditingChModel({required int id, required TimeRecordEditingCommand command, required String targetName, DateTime? creationDateTime, dynamic updateInfo}):
-      _command = command, _updateInfo = updateInfo, super(id: id, targetName: targetName, creationDateTime: creationDateTime ?? DateTime.now());
+  TimeRecordEditingChModel({required int id, required TimeRecordEditingCommand command, required String targetName, required Map<String, dynamic> updateInfo,
+    DateTime? creationDateTime}): _command = command, _updateInfo = updateInfo, super(id: id, targetName: targetName, creationDateTime: creationDateTime ?? DateTime.now());
 
   @override
   String writeUpdateInfoDisplayString({bool useDefaultPreamble = true}){
-    String displayString = "";
+    String displayString = '''Time Record's Date: ${DateTimeUtils().mapDateToDisplayString(_updateInfo["recordDate"])}\n''';
     switch(_command){
       case TimeRecordEditingCommand.create:
         if(useDefaultPreamble){
-          displayString += "Value: ";
+          displayString += "Initial Value: ";
         }
-        displayString += TimeConversionService().fromIntToString(_updateInfo);
+        displayString += TimeConversionService().fromIntToString(_updateInfo["initialValue"]);
         break;
       case TimeRecordEditingCommand.updateName:
         if(useDefaultPreamble){
           displayString += "New Name: ";
         }
-        displayString += _updateInfo;
+        displayString += _updateInfo["newName"];
         break;
       case TimeRecordEditingCommand.updateValue:
         if(useDefaultPreamble){
-          displayString = "Old Value: ${TimeConversionService().fromIntToString(_updateInfo['oldValue'])}\n"
+          displayString += "Old Value: ${TimeConversionService().fromIntToString(_updateInfo['oldValue'])}\n"
           "New Value: ${TimeConversionService().fromIntToString(_updateInfo['newValue'])}";
         }else{
-          displayString = _updateInfo.toString();
+          displayString += _updateInfo.toString();
         }
         break;
       default: break;
--- lib/services/database_initialization_service.dart
+++ lib/services/database_initialization_service.dart
@@ -51,13 +51,13 @@ class DatabaseInitializationService{
           '''INSERT INTO command_history(id_used_command, id_command_type, nm_target, dt_history_creation, ds_update_info) VALUES (1, 1, "Bruxeval", "2024-2-1 15:5:0", NULL),
             (2, 1, "Bruxeval", "2024-2-1 20:12:22", "Grumadzul"), (4, 2, 'Grumadzul', "2023-12-18 19:55:20", "0"), (4, 2, 'Sei la', "2023-12-20 17:12:19", "100"),
             (5, 2, 'Grumadzul', "2023-12-18 20:1:0", "340"), (6, 2, 'Grumadzul', "2023-12-18 21:1:41", "340"),
-            (8, 3, "Bruxeval", "2023-2-2 6:12:38", 'da_record_date:2024-2-2, in_initial_value:1200'),
-            (9, 3, "Grumadzul", "2024-2-2 6:21:4", 'da_record_date:2024-2-2, in_delete_value:600'),
-            (10, 3, "Arstuncio", "2024-2-2 6:41:24", 'da_record_date:2024-2-1, st_new_name:Grumadzul'),
-            (11, 3, "Burxeval", "2023-2-2 7:2:51", 'da_record_date:2024-2-2, in_oldValue:1200, in_newValue:4500');'''
+            (8, 3, "Bruxeval", "2024-2-2 6:12:38", 'da_recordDate:2024-2-2, in_initialValue:1200'),
+            (9, 3, "Grumadzul", "2024-2-2 6:21:4", 'da_recordDate:2024-2-2, in_deleteValue:600'),
+            (10, 3, "Arstuncio", "2024-2-2 6:41:24", 'da_recordDate:2024-2-1, st_newName:Grumadzul'),
+            (11, 3, "Burxeval", "2024-2-2 7:2:51", 'da_recordDate:2024-2-2, in_oldValue:1200, in_newValue:4500');'''
         );
       },
-      version: 17,
+      version: 18,
     );
     CronometerDAO.initialize(_database);
     TimeRecordDAO.initialize(_database);
