310e5a08fdc1f92eef60358488b7af226c5a3d4f
Vanderson Faria Milagre <VandersonMilagre@hotmail.com.br>
2024-02-20 08:40:40 -0300

    Recriação do Esquema de Cores do App Iniciada
    Cores para os elementos principais definidas. Cores para os elementos secundários e de erro ainda não foram testadas.

--- lib/themes/main_theme.dart
+++ lib/themes/main_theme.dart
@@ -4,18 +4,18 @@ abstract class MainTheme{
 
   static ThemeData build(){
     return ThemeData(
-      colorScheme: ColorScheme(
+      colorScheme: const ColorScheme(
         brightness: Brightness.light,
-        primary: Colors.blue[800]!,
-        onPrimary: Colors.amber,
-        secondary: Colors.green,
+        primary: Color.fromARGB(255, 46, 156, 239),
+        onPrimary: Color.fromARGB(255, 255, 245, 54),
+        secondary: Color.fromARGB(255, 50, 213, 96),
         onSecondary: Colors.purple,
-        error: Colors.red,
+        error: Color.fromARGB(255, 255, 56, 56),
         onError: Colors.brown,
-        surface: Colors.black,
-        onSurface: Colors.pink,
-        background: Colors.indigo[300]!,
-        onBackground: Colors.cyan,
+        surface: Color.fromARGB(255, 19, 33, 63),
+        onSurface: Color.fromARGB(255, 242, 245, 255),
+        background: Color.fromARGB(255, 18, 19, 19),
+        onBackground: Color.fromARGB(255, 40, 94, 164),
       ),
     );
   }
