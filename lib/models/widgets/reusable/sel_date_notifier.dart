467b40c1e0d26162b2ec8038c46ca2852989479a
Vanderson Faria Milagre <VandersonMilagre@hotmail.com.br>
2024-02-29 09:20:49 -0300

    Modificação de Comentário do Sel Date Notifier
    Este commit apresenta ERROS DE COMPILAÇÃO. Esta mudança faz parte do commit anterior, mas não tinha sido salva antes dele.

--- lib/models/widgets/reusable/sel_date_notifier.dart
+++ lib/models/widgets/reusable/sel_date_notifier.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-//OBS: Since this class' inheritor scripts are Notifiers, which are written in the same script as their parent Model, they can't be written here. As such, despite all
-//  inheritors having a 'selDate' field, it can't be written here because it would need to be made public for them to access it, since Flutter doesn't have an easy way to
+//OBS: Since this class' extendor scripts are Notifiers, which are written in the same script as their parent Model, they can't be written here. As such, despite all
+//  extendors having a 'selDate' field, it can't be written here because it would need to be made public for them to access it, since Flutter doesn't have an easy way to
 //  create protected fields.
 abstract class SelDateNotifier extends ChangeNotifier{
 
